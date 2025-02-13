7c3b540a00b3a77269e150de7f952db9b9487023
ketakinerkar <ketakinerkar57@gmail.com>
2025-02-13 10:52:02 +0530

    parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path, obj):
     try:
@@ -18,24 +20,38 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
-        report ={}
+        report = {}
 
-        for i in range(len(list(models))):
-            model= list(models.values())[i]
+        for model_name, model in models.items():
+            print(f"Tuning hyperparameters for {model_name}...")
 
-            model.fit(X_train,y_train)
+            # Get parameters for the model
+            para = param.get(model_name, {})
 
-            y_train_pred = model.predict(X_train)
+            if para:  # If there are parameters to tune
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, verbose=1)
+                gs.fit(X_train, y_train)
 
-            y_test_pred = model.predict(X_test)
+                # Use the best model with tuned parameters
+                best_model = gs.best_estimator_
+            else:
+                best_model = model  # If no parameters to tune, use default model
 
-            train_model_score = r2_score(y_train,y_train_pred)
+            # Train the final model
+            best_model.fit(X_train, y_train)
 
-            test_model_score = r2_score(y_test,y_test_pred)
+            # Make predictions
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
 
-            report[list(models.keys())[i]] = test_model_score
+            # Compute R² scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # Store test model score
+            report[model_name] = test_model_score
 
         return report
 
